43e17c27764b7f5d6812e68e103874de87d48ff2
Dmitriy Maygurov <dmitriy.maygurov@gmail.com>
2023-04-14 14:26:45 -0700

    Another attempt

--- script.js
+++ script.js
@@ -22,23 +22,12 @@ function nextQuestion() {
     questionElement.textContent = question;
     answerElement.textContent = answer;
 }
-    
-
-function showAnswer() {
-    const questionElement = document.getElementById("question");
-    const answerElement = document.getElementById("answer");
-
-    questionElement.classList.add("card-flip");
-    answerElement.classList.add("card-flip");
-    answerElement.classList.remove("card-slide");
-}
 
 const cardContainer = document.querySelector(".card-container");
-ccardContainer.addEventListener("click", () => {
+cardContainer.addEventListener("click", () => {
     if (cardContainer.classList.contains("show-answer")) {
         nextQuestion();
     } else {
         cardContainer.classList.add("show-answer");
     }
 });
-
